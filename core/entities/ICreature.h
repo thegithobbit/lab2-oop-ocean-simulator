9f443333719cf241c211f4d42f5a37cf107227c9
osillans <chevikleyla@gmail.com>
2026-06-07 22:43:31 +0300

    Expand creature interface

--- core/entities/ICreature.h
+++ core/entities/ICreature.h
@@ -8,13 +8,17 @@ class ICreature {
 public:
     virtual ~ICreature() = default;
 
-    virtual void update() = 0;
+    virtual void update(int oceanWidth, int oceanHeight) = 0;
     virtual void accept(IVisitor& visitor) = 0;
     virtual std::shared_ptr<ICreature> clone() const = 0;
 
+    virtual int getId() const = 0;
+    virtual void setId(int id) = 0;
     virtual float getX() const = 0;
     virtual float getY() const = 0;
     virtual float getEnergy() const = 0;
+    virtual void setPosition(float x, float y) = 0;
+    virtual void setEnergy(float energy) = 0;
     virtual bool  isAlive() const = 0;
     virtual std::string getType() const = 0;
 };
